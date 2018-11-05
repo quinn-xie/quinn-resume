b4a67e49ba1344d5d0c95290e3b71dd9d5074768
share.xie@aeasycredit.com <share.xie@aeasycredit.com>
2018-11-05 15:33:20 +0800

    修改

--- src/untils/http.js
+++ src/untils/http.js
@@ -1,8 +1,8 @@
 /*
- * @Author: share.xie 
- * @Date: 2018-11-01 10:39:25 
- * @Last Modified by: share.xie
- * @Last Modified time: 2018-11-01 10:42:04
+ * @Author: quinn.xie 
+ * @Date: 2018-11-05 14:48:04 
+ * @Last Modified by:   quinn.xie 
+ * @Last Modified time: 2018-11-05 14:48:04 
  */
 
 'use strict'
